8bd4893a9762ead5a8fbd73b9261382cc6172ba8
horley.monteiro <horley.monteiro@trixti.com.br>
2025-05-10 16:23:00 -0300

    init

---
+++ ios/loguinAllEasy-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  loguinAllEasy-Bridging-Header.h
+//  loguinAllEasy
+//
+//  Created by Horley Leitão Monteiro on 09/05/25.
+//
+
+#ifndef loguinAllEasy_Bridging_Header_h
+#define loguinAllEasy_Bridging_Header_h
+
+
+#endif /* loguinAllEasy_Bridging_Header_h */
